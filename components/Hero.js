5fcf1ecbaeb65fe03bbdcd186f3048041282a010
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-03-29 13:38:17 -0500

    Finished first version of hero section

--- components/Hero.js
+++ components/Hero.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import styles from '../styles/Hero.module.css'
 import Image from 'next/image'
+import Link from 'next/dist/client/link'
 
 const Hero = (props) => {
   return (
     <div className={styles.heroWrapper}>
-        <Image 
+      <Image
         className={styles.heroBG}
         src='/hero_bg.jpg'
-        // width={1920}
-        // height={1080}
         layout='fill'
         objectFit='cover'
-        />
+      />
+      <div className={styles.heroCard}>
+        <h1>Umsatz erhöhen.</h1>
+        <h1>Prozesse automatisieren.</h1>
+        <h1>Kosten optimieren.</h1>
+        <div>
+          <h1 style={{ display: "inline" }}>Alles aus einer </h1>
+          <Link href='/more'>
+            <h1 className={styles.heroHighlight} style={{ display: "inline" }}>Hand</h1>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
