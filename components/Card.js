44c0dc149e1ecaaf2f20bfde84f4356063404b9b
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-03-30 19:36:08 -0500

    Improved appearance and responsiveness of cards

--- components/Card.js
+++ components/Card.js
@@ -1,19 +1,12 @@
 import Image from 'next/image'
 import styles from '../styles/Card.module.css'
-import { useState } from 'react'
 
 
 export const Card = (props) => {
-    const [isReversing, setIsReversing] = useState(false)
-
     return (
         <section>
             <div className={styles.cardWrapper} style={{ width: props.width }}>
-                <div
-                    className={styles.cardBody}
-                    onMouseEnter={()=> setIsReversing(false)}
-                    onMouseLeave={()=> setIsReversing(true)}
-                >
+                <div className={styles.cardBody}>
                     <div className={styles.cardImage}>
                         {props.src ? <Image
                             src={props.src}
