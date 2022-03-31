7a65e54ae8dca192cbdc2a063fda3aa02b832368
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-03-30 19:25:41 -0500

    Finished the hover and unhover animation of card items

--- components/Card.js
+++ components/Card.js
@@ -8,20 +8,24 @@ export const Card = (props) => {
 
     return (
         <section>
-        <div className={styles.cardWrapper} style={{ width: props.width }}>
-            <div className={styles.cardBody}>
-                <div className={styles.cardImage}>
-                    {props.src ? <Image
-                        src={props.src}
-                        height={props.width}
-                        width={props.width}
-                    /> : ''}
-                </div>
-                <div className={styles.cardText}>
+            <div className={styles.cardWrapper} style={{ width: props.width }}>
+                <div
+                    className={styles.cardBody}
+                    onMouseEnter={()=> setIsReversing(false)}
+                    onMouseLeave={()=> setIsReversing(true)}
+                >
+                    <div className={styles.cardImage}>
+                        {props.src ? <Image
+                            src={props.src}
+                            height={props.width}
+                            width={props.width}
+                        /> : ''}
+                    </div>
+                    <div className={styles.cardText}>
                         {props.children}
+                    </div>
                 </div>
             </div>
-        </div>
         </section>
     )
 }
