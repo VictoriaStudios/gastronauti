cb82014bbd8d17112e1838f94baaeccca19a36d2
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-04-05 13:27:06 -0500

    Added a Kassensystem section

---
+++ components/Kassensystem.js
@@ -0,0 +1,15 @@
+import styles from '../styles/Kassensystem.module.css'
+
+const Kassensystem = () => {
+  return (
+      <section className={styles.kassenWrapper}>
+          <h1 className={styles.kassenTitle}></h1>
+          <h2 className={styles.kassenTitle}> </h2>
+          <div className={styles.kassenContent}>
+
+          </div>
+      </section>
+  )
+}
+
+export default Kassensystem
