80d8a5c5b23903b15cce07c308c3293aa34d130d
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-04-07 17:54:49 -0500

    Started implemented a general carousel logic

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,10 +1,28 @@
 import styles from '../styles/Carousel.module.css'
+import { useEffect, useState } from 'react'
 
 const Carousel = (props) => {
+
+  useEffect(() => {
+    if (props.children) {
+      let arrayCount = (props.children.length % 3) + 1
+      console.log (arrayCount)
+
+    }
+
+  
+  }, [props.children])
+  
+
   return (
     <section className={styles.carouselWrapper}>
         <div className={styles.carouselBody}>
-            {props.children}
+            {/* {props.children} */}
+            {props.children.map((element) => (
+              <>
+              {element}
+              </>
+            ))}
         </div>
     </section>
   )
