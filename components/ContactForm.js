53ffd710cb1cf30566ecdd4e68a48b4a1689d21e
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-04-12 12:25:01 -0500

    Continued work on contact form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,23 +1,51 @@
 import { useState } from "react"
+import styles from '../styles/ContactForm.module.css'
 
 
 const ContactForm = () => {
-    const [formContent, setFormContent] = useState ({
-        company:'Firma',
-    })
+  const [formContent, setFormContent] = useState({
+    company: 'Firma',
+    familyName: 'Family Name',
+    firstName: 'First Name',
+    email: 'email',
+    message: ''
+  })
 
-    const handleCompany = (e) => {
-        let newFormContent = {...formContent, company:e.target.value}
-        setFormContent(newFormContent)
-    }
+  const handleCompany = (e) => {
+    let newFormContent = { ...formContent, company: e.target.value }
+    setFormContent(newFormContent)
+  }
+  const handleFamilyName = (e) => {
+    let newFormContent = { ...formContent, familyName: e.target.value }
+    setFormContent(newFormContent)
+  }
+
+  const handleFirstname = (e) => {
+    let newFormContent = { ...formContent, firstName: e.target.value }
+    setFormContent(newFormContent)
+  }
+
+  const handleEmail = (e) => {
+    let newFormContent = { ...formContent, email: e.target.value }
+    setFormContent(newFormContent)
+  }
+
+  const handleMessage = (e) => {
+    let newFormContent = { ...formContent, message: e.target.value }
+    setFormContent(newFormContent)
+  }
 
   return (
-      <>
+    <>
       <h1>Kontakt</h1>
-      <form>
-        <input type='text' name='Firma' value={formContent.company} onChange={handleCompany}/>
+      <form className={styles.formWrapper}>
+        <input className={styles.formLine} type='text' name='company' placeholder={formContent.company} onChange={handleCompany} />
+        <input className={styles.formLine} type='text' name='familyName' placeholder={formContent.familyName} onChange={handleFamilyName}/>
+        <input className={styles.formLine} type='text' name='firstName' placeholder={formContent.firstName} onChange={handleFirstname} />
+        <input className={styles.formLine} type='text' name='email' placeholder={formContent.email} onChange={handleEmail} />
+        <input className={styles.formLine} type='text' name='message' placeholder='Your Message' onChange={handleMessage} />
       </form>
-      </>
+    </>
   )
 }
 
