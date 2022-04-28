ee3183851faef1be492e7d309cf13e2a27998694
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-04-28 18:44:57 -0500

    Added generic business people portraits

--- components/content/quotes.js
+++ components/content/quotes.js
@@ -1,37 +1,37 @@
 export const quotes =  {
     quote01: {
         text: 'Reliable, fast and efficient.',
-        author: 'Markus Fiedermann, BigBusinessPartner.com',
-        icon: '/icon-cogs.png'
+        author: 'Susan Crawford, Bumblebee Hotels',
+        icon: '/portrait01.png'
     },
     quote02: {
         text: 'Good cooperation, excellent service.',
-        author: 'Robert Hauser, Gründwaldhotel Freiberg',
-        icon: '/icon-cogs.png'
+        author: "Melanie Towers, Towers & Co.",
+        icon: '/portrait02.png'
     },
     quote03: {
         text: "Since we started using gastronauti, we have saved 20% in human resources.",
         author: 'Jack Harlon, Seaside Hotel Westenburry',
-        icon: '/icon-cogs.png'
+        icon: '/portrait03.png'
     },
     quote04: {
         text: "The checkout system has made coordination between kitchen and service quick and reliable.",
         author: 'William Knox, Milliford Café, Balticore',
-        icon: '/icon-cogs.png'
+        icon: '/portrait04.png'
     },
     quote05: {
         text: "Before gastronauti, we used three different service providers. The synergies and savings with our new partner are immense.",
-        author: 'Susan Crawford, Bumblebee Hotels',
-        icon: '/icon-cogs.png'
+        author: 'Markus Fiedermann, BigBusinessPartner.com',
+        icon: '/portrait05.png'
     },
     quote06: {
         text: "An excellent and reliable partner.",
         author: "Jesse Pinkerton, Pinkie's Inn",
-        icon: '/icon-cogs.png'
+        icon: '/portrait06.png'
     },
     quote07: {
         text: "Once you have gastronauti's all-in-one solution, you will never want to go back.",
-        author: "Melanie Towers, Towers & Co.",
-        icon: '/icon-cogs.png'
+        author: 'Robert Hauser, Gründwaldhotel Freiberg',
+        icon: '/portrait07.png'
     },
 }
