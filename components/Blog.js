f7b11f581f58e26fc422bc730f11ce1cad51f85e
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-05-03 20:54:49 -0500

    Finished blog expansion and retraction

--- components/Blog.js
+++ components/Blog.js
@@ -7,7 +7,7 @@ import Transition from 'react-transition-group/cjs/Transition'
 const Blog = (props) => {
     const [expanded, setExpanded] = useState(false)
     const transTime = 750
-    const expandtime = 300
+    const expandtime = 350
 
 
 
@@ -19,9 +19,9 @@ const Blog = (props) => {
                     <section onClick={() => setExpanded(!expanded)} className={styles.blogWrapper}>
                         <h1 className={styles.blogTitle}>Blog</h1>
                         <div className={styles.blogContent} style={{
-                            transition: `all ${expandtime}ms ease-out`,
-                            height: state === 'entered' ? '75vh' : '0',
-                            width: state === 'entered' ? '80vw' : '10vw',
+                            transition: `all ${expandtime}ms linear`,
+                            height: state === 'entering' ? '0' : state === 'entered' ? '75vh' : state === 'exiting' ? '0' : state === 'exited' ? '0' : null ,
+                            width: state === 'entering' ? '80vw' : state === 'entered' ? '80vw' : state === 'exiting' ? '80vw' : state === 'exited' ? '10vw' : null ,
                         }}>
                             <h1 style={{
                                 transition: `all ${transTime}ms ease-in`,
