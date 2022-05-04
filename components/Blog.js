db5f7d7f898985c098d18c3d06ca11ebc1a08c2c
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-05-03 20:45:14 -0500

    Improved blog expansion

--- components/Blog.js
+++ components/Blog.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Transition from 'react-transition-group/cjs/Transition'
 
@@ -6,42 +6,34 @@ import Transition from 'react-transition-group/cjs/Transition'
 
 const Blog = (props) => {
     const [expanded, setExpanded] = useState(false)
-    const [blogClass, setBlogClass] = useState(`${styles.blogWrapper}`)
-    const transTime = 800
-    const expandtime = 400
+    const transTime = 750
+    const expandtime = 300
 
-    useEffect(() => {
-        if (!expanded) {
-            setBlogClass(`${styles.blogWrapper} ${styles.blogClickable}`)
-        }
-        else {
-            setBlogClass(`${styles.blogWrapper}`)
-        }
-    }, [expanded])
+
 
 
     return (
-
-        <section onClick={() => setExpanded(!expanded)} className={blogClass}>
-            <h1 className={styles.blogTitle}>Blog</h1>
-            <Transition in={expanded} timeout={transTime}>
-                {state => (
-                    <>
+        <Transition in={expanded} timeout={transTime}>
+            {state => (
+                <>
+                    <section onClick={() => setExpanded(!expanded)} className={styles.blogWrapper}>
+                        <h1 className={styles.blogTitle}>Blog</h1>
                         <div className={styles.blogContent} style={{
-                            transition: `all ${expandtime}ms ease-in`,
-                            height: state === 'entered' ? '65vh' : '0'
+                            transition: `all ${expandtime}ms ease-out`,
+                            height: state === 'entered' ? '75vh' : '0',
+                            width: state === 'entered' ? '80vw' : '10vw',
+                        }}>
+                            <h1 style={{
+                                transition: `all ${transTime}ms ease-in`,
+                                opacity: state === 'entered' ? 1 : 0
                             }}>
-                                <h1 style={{
-                                    transition: `all ${transTime}ms ease-in`,
-                                    opacity: state === 'entered' ? 1 : 0
-                                }}>
-                                    Content
-                                </h1>
+                                Content
+                            </h1>
                         </div>
-                    </>
-                )}
-            </Transition>
-        </section>
+                    </section>
+                </>
+            )}
+        </Transition>
     )
 }
 
