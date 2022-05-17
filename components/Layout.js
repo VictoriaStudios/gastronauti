fb8da832e3bb547df876f49f7d660e11c7663e1f
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-05-16 20:13:59 -0500

    Fixing bugs

--- components/Layout.js
+++ components/Layout.js
@@ -4,31 +4,44 @@ import styles from '../styles/Layout.module.css'
 
 const Layout = (props, ref) => {
   const [navSticky, setNavSticky] = useState(false)
+  let headerSize = 0
   const headerRef = useRef()
 
   const getHeaderSize = () => {
     if (!headerRef.current) return
-    console.log (headerRef.current.offsetHeight)
+    return headerRef.current.offsetHeight
   }
 
   const handleScroll = () => {
+    let shouldBeSticky = false
     console.log (window.scrollY)
-    console.log ("Headersize: ")
-    getHeaderSize()
+    if (window.scrollY >= headerSize) {
+      console.log ("Should be sticky")
+      shouldBeSticky = true
+    }
+    else {
+      console.log ("Shouldn't be sticky")
+      shouldBeSticky = false
+    }
+    console.log ("navsticky is " + navSticky)
+    if (shouldBeSticky && !navSticky) setNavSticky (true)
+    else if (!shouldBeSticky && navSticky) setNavSticky (false)
+
   }
 
   useEffect(() => {
+    headerSize = getHeaderSize()
     window.addEventListener("scroll", handleScroll);
     return () => {
-        window.removeEventListener("scroll", handleScroll);
-    };
-}, []);
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, [])
 
   return (
     <>
       <header ref={headerRef}>
-        <div className={styles.stickyDivOff}/>
-        <Navbar ref={ref} />
+        <div className={styles.stickyDivOff} />
+        {!navSticky ? <Navbar ref={ref} /> : null}
       </header>
       {props.children}
       <footer className='placeholder'>
