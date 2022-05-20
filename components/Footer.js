571169c7cdf757601e09122676d17937864a37c5
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-05-20 09:11:37 -0500

    Finished Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,34 @@
 import styles from '../styles/Footer.module.css'
 import { scrollTo } from '../utilities/scrollTo'
+import Image from 'next/image'
 
 
 const Footer = (props) => {
+    const logoSize = 200
     return (
         <footer className={styles.footerWrapper}>
-            <a className={styles.navElement} onClick={() => scrollTo(props.scrollRefs.kassenRef)}> Modules </a>
-            <a className={styles.navElement} onClick={() => scrollTo(props.scrollRefs.blogRef)}> Blog </a>
-            <a className={styles.navElement} onClick={() => scrollTo(props.scrollRefs.empfehlungsRef)}> Recommendations </a>
-            <a className={styles.navElement} onClick={() => scrollTo(props.scrollRefs.kontaktRef)}> Contact </a>
+            <section className={styles.footerBody}>
+                <div className={styles.footerLogoWrapper}>
+                    <Image
+                        src='/logo.png'
+                        width={logoSize}
+                        height={logoSize}
+
+                    />
+                </div>
+                <div style={{ width: '10%' }} />
+                <div className={styles.footerNav}>
+                    <a className={styles.navElement} onClick={() => scrollTo(props.scrollRefs.kassenRef)}> Modules </a>
+                    <a className={styles.navElement} onClick={() => scrollTo(props.scrollRefs.blogRef)}> Blog </a>
+                    <a className={styles.navElement} onClick={() => scrollTo(props.scrollRefs.empfehlungsRef)}> Recommendations </a>
+                    <a className={styles.navElement} onClick={() => scrollTo(props.scrollRefs.kontaktRef)}> Contact </a>
+                </div>
+            </section>
+            <hr style={{ borderColor: '#DBD6CC'}}/>
+            <h4 className={styles.footerSignature}>© Marcel Bruna 2022</h4>
+
         </footer>
+
     )
 }
 
