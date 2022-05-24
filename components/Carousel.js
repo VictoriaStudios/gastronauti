f1459e460b3ebaec4e273f21bcefbdd2eca3dd71
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-05-24 11:36:56 -0500

    Implemented a useWindowDimemnsion hook

--- components/Carousel.js
+++ components/Carousel.js
@@ -2,12 +2,15 @@ import styles from '../styles/Carousel.module.css'
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Transition from 'react-transition-group/cjs/Transition'
+import useWindowDimensions from './utils/useWindowDimensions'
 
 const Carousel = (props) => {
   const [allElements, setAllElements] = useState([])
   const [currElements, setCurrElements] = useState([])
   const [shownIndex, setShownIndex] = useState(0)
   const [showElements, setShowElements] = useState(true)
+  const {width, height} = useWindowDimensions()
+  let visAmount = 3
   const transTime = 300
 
   const handleNext = () => {
@@ -35,14 +38,15 @@ const Carousel = (props) => {
   }
 
   useEffect(() => {
+
     if (props.children) {
       //create a two dimensional array, where the first dimension is the index of the three elements to be shown
       let preppedArray = []
       let currIndex = -1
       for (let i = 0; i < props.children.length; i++) {
-        if (i % 3 === 0) {
+        if (i % visAmount === 0) {
           currIndex++
-          preppedArray.push(props.children.slice(i, i + 3))
+          preppedArray.push(props.children.slice(i, i + visAmount))
         }
       }
       setCurrElements(preppedArray[shownIndex])
@@ -50,7 +54,7 @@ const Carousel = (props) => {
     }
 
 
-  }, [props.children])
+  }, [props.children, shownIndex])
 
 
 
