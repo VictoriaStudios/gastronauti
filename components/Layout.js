f821056e530356e28840f19b57bc27b2d4025039
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-05-26 15:57:54 -0500

    Started work on a mobile nav sidebar

--- components/Layout.js
+++ components/Layout.js
@@ -3,10 +3,14 @@ import Navbar from './Navbar'
 import styles from '../styles/Layout.module.css'
 import Transition from 'react-transition-group/cjs/Transition'
 import Footer from './Footer'
+import Sidebar from './Sidebar'
+import useWindowDimensions from './utils/useWindowDimensions'
 
 
 const Layout = (props, ref) => {
   const [navSticky, setNavSticky] = useState(false)
+  const [sidebarVis, setSidebarVis] = useState(false)
+  const {width, height} = useWindowDimensions()
   let headerSize = 0
   const headerRef = useRef()
   const transTime = 200
@@ -37,7 +41,8 @@ const Layout = (props, ref) => {
   return (
     <>
       <header ref={headerRef}>
-        {!navSticky ? <Navbar ref={ref} /> : <div style={{ visibility: 'hidden' }}><Navbar ref={ref} /></div>}
+        <Sidebar/>
+        {!navSticky && !sidebarVis ? <Navbar ref={ref} /> : <div style={{ visibility: 'hidden' }}><Navbar ref={ref} /></div>}
         <Transition in={navSticky} timeout={transTime}>
           {state => (
             <>
---
+++ components/Sidebar.js
@@ -0,0 +1,10 @@
+import Transition from "react-transition-group/cjs/Transition"
+import styles from '../styles/Sidebar.module.css'
+
+const Sidebar = (props) => {
+  return (
+    <aside className={styles.sidebarWrapper}>This is the sidebar</aside>
+  )
+}
+
+export default Sidebar
