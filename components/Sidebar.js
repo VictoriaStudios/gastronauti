ee69c392eeb8a51f1ab845ba6f82b3bb04dd03a4
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-05-28 13:35:33 -0500

    Minor bug fix

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,7 +11,7 @@ const Sidebar = (props) => {
     }}
       className={styles.sidebarWrapper}>
       <div className={styles.sidebarIcon}>
-        <svg onClick={props.onClick} width="20" height="20" version="1.1" viewBox="0 0 13.229 13.229" xmlns="http://www.w3.org/2000/svg"><g fill="none" stroke="#A79878" stroke-width="1.3229"><path d="m1.6964 2.3806h9.8363" /><path d="m1.6964 6.6146h9.8363" /><path d="m1.6964 10.849h9.8363" /></g></svg>
+        <svg onClick={props.onClick} width="20" height="20" version="1.1" viewBox="0 0 13.229 13.229" xmlns="http://www.w3.org/2000/svg"><g fill="none" stroke="#A79878" strokeWidth="1.3229"><path d="m1.6964 2.3806h9.8363" /><path d="m1.6964 6.6146h9.8363" /><path d="m1.6964 10.849h9.8363" /></g></svg>
       </div>
       {props.children}
     </aside>
